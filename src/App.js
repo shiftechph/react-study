9580df2e05eebf7c36a4899c9ca41e7376f77f9b
ReymarkTion <reymark.tion@fligno.com>
2020-10-11 21:33:03 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+const ErwinKent = (props) => {
+
+  console.log(props.nameLast);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <h1>{ props.nameLast }</h1>
+  )
+};
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        name: "Erwin",
+        last: "Jay",
+
+        showReset: false
+    }
+  }
+
+  buttonClick = () => {
+    this.setState({ 
+      name: "Kent",
+      showReset: true
+    });
+  }
+
+  buttonReset = () => {
+    this.setState({ 
+      name: "Erwin",
+      showReset: false
+    });
+  }
+
+  goToLogin = () => {
+    this.props.history.push('/login-screen');
+  }
+
+  render() {
+
+    return (
+      <div>
+
+        { `${this.state.name} ${this.state.last}` }
+
+        <br/>
+        <br/>
+
+        {
+          !this.state.showReset
+            ?
+            <button 
+              type="button" 
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={this.buttonClick}
+              >
+                Click me!
+            </button>
+            :
+            <button 
+              type="button" 
+              className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={this.buttonReset}
+              >
+                Reset!
+            </button>
+        }
+
+        <br/>
+        <br/>
+
+        <ErwinKent nameLast={`${this.state.name} ${this.state.last}`} />
+
+        <br/> <br/>
+
+        <button
+          type="button"
+          className={`bg-red-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded`}
+          onClick={this.goToLogin}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Go To Login
+        </button>
+
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/tailwind.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Route, Router, Switch, Redirect } from 'react-router-dom';
+
+import { history } from './helpers';
+
+import { LoginPage } from './pages/login';
+
+const MainRoute = () => {
+
+  return (
+    <Router history={history}>
+      <Switch>
+        
+        <Route path={'/login-screen'} component={ LoginPage } />
+
+        <Route path={'/'} component={ App } />
+      
+      </Switch>
+    </Router>
+  )
+
+}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainRoute />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
