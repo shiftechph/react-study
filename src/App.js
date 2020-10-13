a189419185d7776ae255f8a7653f510341afd52e
ReymarkTion <reymark.tion@fligno.com>
2020-10-13 12:01:58 +0800

    added tab

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ class App extends Component {
 
         // button tick
         buttonTick: false,
+
+        buttonTab: 1
     }
   }
 
@@ -104,6 +106,65 @@ class App extends Component {
         >
           Tick Button
         </button>
+
+        <br/> <br/>
+
+        <hr/>
+
+        <br/> <br/>
+
+        <div className="flex flex-row">
+        
+          <button
+            type="button"
+            className={`text-white font-bold py-2 px-4 rounded ${this.state.buttonTab == 1 ? `bg-green-900` : `bg-gray-500`}`}
+            onClick={() => this.setState({ buttonTab: 1 })}
+          >
+            Button 1
+          </button>
+
+          <button
+          type="button"
+          className={`text-white font-bold py-2 px-4 rounded ${this.state.buttonTab == 2 ? `bg-green-900` : `bg-gray-500`}`}
+          onClick={() => this.setState({ buttonTab: 2 })}
+        >
+          Button 2
+        </button>
+
+        <button
+          type="button"
+          className={`text-white font-bold py-2 px-4 rounded ${this.state.buttonTab == 3 ? `bg-green-900` : `bg-gray-500`}`}
+          onClick={() => this.setState({ buttonTab: 3 })}
+        >
+          Button 3
+        </button>
+
+        </div>
+
+        <br/>
+
+        {
+          this.state.buttonTab == 1 &&
+            <div className={`bg-pink-400 w-64 h-64`}>
+              Tab 1
+            </div> 
+        }
+
+        {
+          this.state.buttonTab == 2 &&
+            <div className={`bg-pink-400 w-64 h-64`}>
+              Tab 2
+            </div> 
+        }
+
+        {
+          this.state.buttonTab == 3 &&
+            <div className={`bg-pink-400 w-64 h-64`}>
+              Tab 3
+            </div> 
+        }
+
+        <br/><br/>
 
       </div>
     )
